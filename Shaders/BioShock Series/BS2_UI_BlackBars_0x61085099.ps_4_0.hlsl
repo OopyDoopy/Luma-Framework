ff91d221f7237a875ec93f8b96a5ed766f76c4c1
garamond13 <98652255+garamond13@users.noreply.github.com>
2026-02-22 19:11:23 +0100

    BioShock 2: Actually fix black bars

--- Shaders/BioShock Series/BS2_UI_BlackBars_0x61085099.ps_4_0.hlsl
+++ Shaders/BioShock Series/BS2_UI_BlackBars_0x61085099.ps_4_0.hlsl
@@ -11,10 +11,7 @@ void main(
   //
   // Besides black bars this renders some elenments on the map,
   // including black elemets. But it may not be a significant issue (eg. black outline to red arrow).
-  if (length(v1.xyz) <= 1e-6) {
-    o0.xyzw = 0;
-  }
-  o0.xyzw = v1.xyzw;
+  o0.xyzw = length(v1.xyz) <= 1e-6 ? 0.0 : v1.xyzw;
 #else
   o0.xyzw = v1.xyzw;
 #endif
